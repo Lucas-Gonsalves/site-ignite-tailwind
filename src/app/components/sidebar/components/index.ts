fea4e93c73bcc48593e603a2a2166a2f9b4a6d00
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-09 18:24:31 -0300

    feat(sidebar): create components   New components were created for the sidebar: settingsNavigation and UsedSpacedWidget. The sidebar was refactored for organizational purposes, and settings were added to the nav-item component.

--- src/app/components/sidebar/components/index.ts
+++ src/app/components/sidebar/components/index.ts
@@ -1,2 +1,5 @@
 export * from "./logo";
 export * from "./main-navigation";
+export * from "./nav-item";
+export * from "./settings-navigation";
+export * from "./used-space-widget";
--- src/app/components/sidebar/sidebar.tsx
+++ src/app/components/sidebar/sidebar.tsx
@@ -1,10 +1,15 @@
 import { SearchIcon } from "lucide-react";
 
-import { Logo, MainNavigation } from "./components";
+import {
+  Logo,
+  MainNavigation,
+  SettingsNavigation,
+  UsedSpacedWidget,
+} from "./components";
 
 export const Sidebar = () => {
   return (
-    <aside className="space-y-6 border-r-2 border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r-2 border-zinc-200 px-5 py-8">
       <Logo />
       <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
         <SearchIcon className="h-5 w-5 text-zinc-500" />
@@ -14,6 +19,11 @@ export const Sidebar = () => {
         />
       </div>
       <MainNavigation />
+
+      <section className="mt-auto flex max-h-fit flex-col gap-6">
+        <SettingsNavigation />
+        <UsedSpacedWidget />
+      </section>
     </aside>
   );
 };
