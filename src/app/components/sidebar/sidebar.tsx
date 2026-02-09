fcfc28148d92cc6e788f8959a6d95736b1873868
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-09 15:09:52 -0300

    feat(sidebar): create search input   The search input was created and styled to match the aside.

--- src/app/components/sidebar/sidebar.tsx
+++ src/app/components/sidebar/sidebar.tsx
@@ -1,10 +1,18 @@
+import { Search } from "lucide-react";
+
 import { Logo } from "./logo";
 
 export function Sidebar() {
   return (
-    <aside className="border-r-2 border-zinc-200 px-5 py-8">
+    <aside className="space-y-6 border-r-2 border-zinc-200 px-5 py-8">
       <Logo />
-      <div className=""></div>
+      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+        <Search className="h-5 w-5 text-zinc-500" />
+        <input
+          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+          placeholder="Seach"
+        />
+      </div>
     </aside>
   );
 }
