fd4d3aaa654a876c7bb3115727f01253dc81877c
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-13 02:30:42 -0300

    feat(form): create file-photo input   Created and styled the input responsible for importing the user's photo file.

--- src/templates/home-template/components/form/form.tsx
+++ src/templates/home-template/components/form/form.tsx
@@ -1,4 +1,4 @@
-import { Mail } from "lucide-react";
+import { MailIcon, UploadCloudIcon, UserIcon } from "lucide-react";
 
 import { Button } from "@/components/button";
 import { Input } from "@/components/input";
@@ -45,13 +45,13 @@ export const Form = () => {
             id="email"
             type="email"
             defaultValue="lucaseluz@gmail.com"
-            icon={Mail}
+            icon={MailIcon}
           />
         </div>
 
         <div className="grid-cols-settings-form grid gap-3 pb-5">
           <label
-            htmlFor="your-photo"
+            htmlFor="file-photo"
             className="text-sm font-medium text-zinc-700"
           >
             Your photo
@@ -59,7 +59,33 @@ export const Form = () => {
               This will be displayed on your profile.
             </span>
           </label>
-          <div></div>
+          <div className="flex items-start gap-5">
+            <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
+              <UserIcon className="h-8 w-8 text-violet-500" />
+            </div>
+
+            <label
+              htmlFor="file-photo"
+              className="group hover:bg-violet-25 flex flex-1 cursor-pointer flex-col items-center justify-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm transition-colors duration-200 hover:border-violet-200 hover:text-violet-500"
+            >
+              <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 transition-colors duration-200 group-hover:border-violet-50 group-hover:bg-violet-100">
+                <UploadCloudIcon className="h-5 w-5 text-zinc-600 transition-colors duration-200 group-hover:text-violet-600" />
+              </div>
+
+              <div className="flex flex-col items-center gap-1">
+                <span className="text-sm">
+                  <strong className="font-semibold text-violet-700">
+                    Click to upload
+                  </strong>{" "}
+                  or drag and drop
+                </span>
+                <span className="text-sm">
+                  SVG, PNG, JPG, or GIF (max. 800x400px)
+                </span>
+              </div>
+            </label>
+            <input type="file" className="sr-only" id="file-photo" />
+          </div>
         </div>
 
         <div className="grid-cols-settings-form grid gap-3 pb-5">
