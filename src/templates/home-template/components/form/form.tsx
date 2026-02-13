14c9f7e1b78b514f218f811150ffe1d199fc7a78
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-13 01:56:54 -0300

    feat(form): create inputs structure   The main structure of the inputs for the form component has been created.

--- src/templates/home-template/components/form/form.tsx
+++ src/templates/home-template/components/form/form.tsx
@@ -1,4 +1,7 @@
+import { Mail } from "lucide-react";
+
 import { Button } from "@/components/button";
+import { Input } from "@/components/input";
 
 export const Form = () => {
   return (
@@ -12,14 +15,108 @@ export const Form = () => {
         </div>
         <div className="flex items-center gap-2">
           <Button type="button" title="Cancel" variant="empty" />
-          <Button type="button" title="Save" />
+          <Button id="settings" type="submit" title="Save" />
         </div>
       </div>
+
       <form
         action=""
         id="settings"
-        className="mt-6 flex w-full flex-col"
-      ></form>
+        className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200"
+      >
+        <div className="grid-cols-settings-form grid gap-3 pb-5">
+          <label
+            htmlFor="first-name"
+            className="text-sm font-medium text-zinc-700"
+          >
+            Name
+          </label>
+          <div className="grid grid-cols-2 gap-6">
+            <Input id="first-name" defaultValue="Lucas" />
+            <Input id="last-name" defaultValue="Gonçalves" />
+          </div>
+        </div>
+
+        <div className="grid-cols-settings-form grid gap-3 pb-5">
+          <label htmlFor="email" className="text-sm font-medium text-zinc-700">
+            Email address
+          </label>
+          <Input
+            id="email"
+            type="email"
+            defaultValue="lucaseluz@gmail.com"
+            icon={Mail}
+          />
+        </div>
+
+        <div className="grid-cols-settings-form grid gap-3 pb-5">
+          <label
+            htmlFor="your-photo"
+            className="text-sm font-medium text-zinc-700"
+          >
+            Your photo
+            <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+              This will be displayed on your profile.
+            </span>
+          </label>
+          <div></div>
+        </div>
+
+        <div className="grid-cols-settings-form grid gap-3 pb-5">
+          <label htmlFor="role" className="text-sm font-medium text-zinc-700">
+            Role
+          </label>
+          <Input id="role" defaultValue="CTO" />
+        </div>
+
+        <div className="grid-cols-settings-form grid gap-3 pb-5">
+          <label
+            htmlFor="country"
+            className="text-sm font-medium text-zinc-700"
+          >
+            Country
+          </label>
+          <div></div>
+        </div>
+
+        <div className="grid-cols-settings-form grid gap-3 pb-5">
+          <label
+            htmlFor="time-zone"
+            className="text-sm font-medium text-zinc-700"
+          >
+            Time zone
+          </label>
+          <div></div>
+        </div>
+
+        <div className="grid-cols-settings-form grid gap-3 pb-5">
+          <label htmlFor="bio" className="text-sm font-medium text-zinc-700">
+            Bio
+            <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+              Write a short introduction.
+            </span>
+          </label>
+          <div></div>
+        </div>
+
+        <div className="grid-cols-settings-form grid gap-3 pb-5">
+          <label
+            htmlFor="portfolio-projects"
+            className="text-sm font-medium text-zinc-700"
+          >
+            Portfolio projects
+            <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+              share a few snippets of your work.
+            </span>
+          </label>
+          <div></div>
+        </div>
+
+        <div className="flex items-center justify-end gap-2 pt-5">
+          <Button type="button" title="Cancel" variant="empty" />
+          <Button type="submit" title="Save" />
+        </div>
+      </form>
     </div>
   );
 };
--- src/templates/home-template/home-template.tsx
+++ src/templates/home-template/home-template.tsx
@@ -1,10 +1,12 @@
 import { SettingsTabs } from "./components";
+import { Form } from "./components/form";
 
 export const HomeTemplate = () => {
   return (
     <div>
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
       <SettingsTabs />
+      <Form />
     </div>
   );
 };
